bd789ce5ca441ab97d907cb2d8d7843e5db77dff
Deepaky29 <c0158028@gmail.com>
2026-02-06 00:53:45 +0530

    Update server.js

--- Hrbackend/server.js
+++ Hrbackend/server.js
@@ -8,7 +8,10 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://h-rmini.vercel.app/",
+    credentials: true
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URL)

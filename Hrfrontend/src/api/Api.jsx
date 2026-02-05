6dd0448f862e84c1d4fa9ad130de7ab057b83f38
Deepaky29 <c0158028@gmail.com>
2026-02-06 01:07:37 +0530

    Update Api.jsx

--- Hrfrontend/src/api/Api.jsx
+++ Hrfrontend/src/api/Api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hrmini-h4aa.onrender.com/",
+  baseURL: "https://hrmini-h4aa.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
